e813f298a76c4ffb91d44629dc5b97fca44d35d0
Xuan Zhu <xzhu@e1z4r9p1.42.us.org>
2018-10-26 17:32:01 -0700

    sep done, oct before exam00

--- get_logins.sh
+++ get_logins.sh
@@ -1,1 +1,1 @@
-cat allresult.txt |  grep "2018/8" | sed 's/  .*//' | sed 's/.* //' | sort -u > aug2018.csv
+cat result.txt |  grep "2018/10" | sed 's/  .*//' | sed 's/.* //' | sort -u > oct2018.csv
